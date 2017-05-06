2d5e53632376b3646c56b3b2ea42c71cd82f276e
Aaron Davidson <Aaron Davidson>
2017-05-06 19:01:43 -0400

    Add project files.

---
+++ IoTMonitor/IoTMonitor/App.cs
@@ -0,0 +1,19 @@
+﻿using IoTMonitor.Helpers;
+using IoTMonitor.Interfaces;
+using IoTMonitor.Services;
+using IoTMonitor.Model;
+
+namespace IoTMonitor
+{
+    public partial class App
+    {
+        public App()
+        {
+        }
+
+        public static void Initialize()
+        {
+            ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
+        }
+    }
+}
