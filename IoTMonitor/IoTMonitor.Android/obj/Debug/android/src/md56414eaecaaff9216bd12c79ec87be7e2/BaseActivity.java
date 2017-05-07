d08586f6f82d4c279c22fa57b81bb84c366970b1
codenize <info@codenize.com>
2017-05-07 02:56:09 -0400

    initial commit

---
+++ IoTMonitor/IoTMonitor.Android/obj/Debug/android/src/md56414eaecaaff9216bd12c79ec87be7e2/BaseActivity.java
@@ -0,0 +1,47 @@
+package md56414eaecaaff9216bd12c79ec87be7e2;
+
+
+public class BaseActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("IoTMonitor.Droid.BaseActivity, IoTMonitor.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", BaseActivity.class, __md_methods);
+	}
+
+
+	public BaseActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == BaseActivity.class)
+			mono.android.TypeManager.Activate ("IoTMonitor.Droid.BaseActivity, IoTMonitor.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ IoTMonitor/IoTMonitor.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("IoTMonitor.Droid.MainApplication, IoTMonitor.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md56414eaecaaff9216bd12c79ec87be7e2.MainApplication.class, md56414eaecaaff9216bd12c79ec87be7e2.MainApplication.__md_methods);
+		
+	}
+}
