16a289327be02be29b095c43e009d25b9501e11c
codenize <info@codenize.com>
2017-10-22 17:47:16 -0400

    not sure what changed but committing anyway

---
+++ IoTMonitor/IoTMonitor.UWP/Pages/RestApi/Docs/RestApiCSharp.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using AppStudio.DataProviders.Core;
+using AppStudio.DataProviders.RestApi;
+using AppStudio.DataProviders;
+
+
+namespace AppStudio.Uwp.Samples
+{
+    public sealed partial class RestApiSample : Page
+    {
+        private RestApiDataProvider _dataProvider;
+
+        public RestApiSample()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<object> Items
+        {
+            get { return (ObservableCollection<object>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsProperty = DependencyProperty
+            .Register("Items", typeof(ObservableCollection<object>), typeof(RestApiSample), new PropertyMetadata(null));
+
+
+        public async void GetItemsTokenPagination()
+        {
+            string endPoint = "http://MyRestApiEndPoint.com";
+            var maxRecordsParam = 20;
+            var paginationParameterName = "token";
+            var pageSizeParemeterName = "limit";
+            var responseTokenName = "meta.next_token";
+            var orderByParameterName = "order_by";
+            var orderByParameterValue = "date";
+            var orderDirectionParameterName = "order";
+            var orderDirectionParameterValue = "DESC";
+
+
+            var paginationConfig = new TokenPagination(paginationParameterName, responseTokenName, pageSizeParemeterName)
+            {
+                OrderByParameterName = orderByParameterName,
+                OrderByParameterValue = orderByParameterValue,
+                OrderDirectionParameterName = orderDirectionParameterName,
+                OrderDirectionParameterValue= orderDirectionParameterValue
+            };
+
+
+            var config = new RestApiDataConfig()
+            {
+                Url = new Uri(endPoint),
+                PaginationConfig = paginationConfig
+            };
+
+            var parser = new JsonParser<MySchema>();
+            _dataProvider = new RestApiDataProvider();
+            var items = await _dataProvider.LoadDataAsync(config, maxRecordsParam, parser);
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public async void GetItemsPageNumberPagination()
+        {
+            string endPoint = "http://MyRestApiEndPoint.com";
+            var maxRecordsParam = 20;
+            var paginationParameterName = "page";
+            var pageSizeParemeterName = "limit";
+            var orderByParameterName = "order_by";
+            var orderByParameterValue = "date";
+            var orderDirectionParameterName = "order";
+            var orderDirectionParameterValue = "DESC";
+
+            var paginationConfig = new PageNumberPagination(paginationParameterName, false, pageSizeParemeterName)
+            {
+                OrderByParameterName = orderByParameterName,
+                OrderByParameterValue = orderByParameterValue,
+                OrderDirectionParameterName = orderDirectionParameterName,
+                OrderDirectionParameterValue = orderDirectionParameterValue
+            };
+
+            var config = new RestApiDataConfig()
+            {
+                Url = new Uri(endPoint),
+                PaginationConfig = paginationConfig
+            };
+
+            var parser = new JsonParser<MySchema>();
+            _dataProvider = new RestApiDataProvider();
+            var items = await _dataProvider.LoadDataAsync(config, maxRecordsParam, parser);
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public async void GetItemsItemOffsetPagination()
+        {
+            string endPoint = "http://MyRestApiEndPoint.com";
+            var maxRecordsParam = 20;
+            var offsetParemeterName = "offset";
+            var pageSizeParemeterName = "limit";
+            var orderByParameterName = "order_by";
+            var orderByParameterValue = "date";
+            var orderDirectionParameterName = "order";
+            var orderDirectionParameterValue = "DESC";
+
+
+            var paginationConfig = new ItemOffsetPagination(offsetParemeterName, true, pageSizeParemeterName, maxRecordsParam)
+            {
+                OrderByParameterName = orderByParameterName,
+                OrderByParameterValue = orderByParameterValue,
+                OrderDirectionParameterName = orderDirectionParameterName,
+                OrderDirectionParameterValue = orderDirectionParameterValue
+            };
+
+            var config = new RestApiDataConfig()
+            {
+                Url = new Uri(endPoint),
+                PaginationConfig = paginationConfig
+            };
+
+            var parser = new JsonParser<MySchema>();
+            _dataProvider = new RestApiDataProvider();
+            var items = await _dataProvider.LoadDataAsync(config, maxRecordsParam, parser);
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public async void GetItemsNextUrlPagination()
+        {
+            string endPoint = "http://MyRestApiEndPoint.com";
+            var maxRecordsParam = 20;
+            var responseTokenName = "meta.next_page";
+            var pageSizeParemeterName = "limit";
+            var orderByParameterName = "order_by";
+            var orderByParameterValue = "date";
+            var orderDirectionParameterName = "order";
+            var orderDirectionParameterValue = "DESC";
+
+
+            var paginationConfig = new NextPageUrlPagination(responseTokenName, pageSizeParemeterName)
+            {
+                OrderByParameterName = orderByParameterName,
+                OrderByParameterValue = orderByParameterValue,
+                OrderDirectionParameterName = orderDirectionParameterName,
+                OrderDirectionParameterValue = orderDirectionParameterValue
+            };
+
+            var config = new RestApiDataConfig()
+            {
+                Url = new Uri(endPoint),
+                PaginationConfig = paginationConfig
+            };
+
+            var parser = new JsonParser<MySchema>();
+            _dataProvider = new RestApiDataProvider();
+            var items = await _dataProvider.LoadDataAsync(config, maxRecordsParam, parser);
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+
+        private async void GetMoreItems()
+        {
+            var items = await _dataProvider.LoadMoreDataAsync<MySchema>();
+
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+
+    public class MySchema : SchemaBase
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+    }
+}
